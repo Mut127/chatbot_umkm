f7100d1f43f5f10a8a7b895f4f41c8e7c5d062ff
Muthia Khanza <121779497+Mut127@users.noreply.github.com>
2026-03-02 23:25:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -885,6 +885,6 @@ def kbli_kategori_page(kategori):
     )
 
 
-# Run Server
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
